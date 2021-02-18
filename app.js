dfbb071139ef3970dc7a454971a7cf20c97cb2a9
Rares Sandu <sandurares57@gmail.com>
2021-02-18 09:01:29 +0200

    Starting my coding challange journey of 100 days

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+CODING CHALLANGE 1
+Return the Sum of Two Numbers
+Create a function that takes two numbers as arguments and return their sum.
+
+Examples
+addition(3, 2) ➞ 5
+
+addition(-3, -6) ➞ -9
+
+addition(7, 3) ➞ 10
+*/
+
+function addition(a, b) {
+	return a + b;
+}
