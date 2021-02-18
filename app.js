be13f8062ce68ef9909770002d5a00b6482865ae
Rares Sandu <sandurares57@gmail.com>
2021-02-18 09:06:41 +0200

    Second challange

--- app.js
+++ app.js
@@ -14,3 +14,22 @@ addition(7, 3) ➞ 10
 function addition(a, b) {
 	return a + b;
 }
+
+/* CODING CHALLANGE 2
+
+Return the First Element in an Array
+
+Create a function that takes an array containing only numbers and return the first element.
+
+Examples
+getFirstValue([1, 2, 3]) ➞ 1
+
+getFirstValue([80, 5, 100]) ➞ 80
+
+getFirstValue([-500, 0, 50]) ➞ -500
+
+*/
+
+function getFirstValue(arr) {
+	return arr[0];
+}
