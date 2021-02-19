7d49657de59cf918cd2e0c52d6a8f1f8f858a618
Rares Sandu <sandurares57@gmail.com>
2021-02-19 09:54:20 +0200

    day 2 of coding challanges

--- app.js
+++ app.js
@@ -33,3 +33,89 @@ getFirstValue([-500, 0, 50]) ➞ -500
 function getFirstValue(arr) {
 	return arr[0];
 }
+
+/* CODING CHALLANGE 3
+
+Fix the Expression
+
+Fix the code in the Code tab so the function returns true if and only if x is equal to 7. Try to debug code and pass all the tests.
+
+Examples
+isSeven(4) ➞ false
+
+isSeven(9) ➞ false
+
+isSeven(7) ➞ true
+
+*/
+
+function isSeven(x) {
+	if (x === 7) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/* CODING CHALLANGE 4
+
+Convert Hours into Seconds
+
+Write a function that converts hours into seconds.
+
+Examples
+howManySeconds(2) ➞ 7200
+
+howManySeconds(10) ➞ 36000
+
+howManySeconds(24) ➞ 86400
+
+*/
+
+function howManySeconds(hours) {
+	return (hours = hours * 3600);
+}
+
+/*
+ CODING CHALLANGE 5
+
+ Convert Age to Days
+
+ Create a function that takes the age and return the age in days.
+
+Examples
+calcAge(65) ➞ 23725
+
+calcAge(0) ➞ 0
+
+calcAge(20) ➞ 7300
+
+Notes
+Use 365 days as the length of a year for this challenge.
+Ignore leap years and days between last birthday and now.
+Expect only positive integer inputs.
+*/
+
+function calcAge(age) {
+	return (age = age * 365);
+}
+
+/*
+ CODING CHALLANGE 6
+
+ Find the Perimeter of a Rectangle
+
+ Create a function that takes length and width and finds the perimeter of a rectangle.
+
+Examples
+findPerimeter(6, 7) ➞ 26
+
+findPerimeter(20, 10) ➞ 60
+
+findPerimeter(2, 9) ➞ 22
+
+*/
+
+function findPerimeter(length, width) {
+	return (x = 2 * (length + width));
+}
