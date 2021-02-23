f223260111bf1b68b5ee80dbef0beb8b077728fb
Rares Sandu <sandurares57@gmail.com>
2021-02-23 10:44:09 +0200

    need to find new platform, triel is over

--- app.js
+++ app.js
@@ -246,3 +246,71 @@ function minMax(arr) {
 	a.push(Math.max(...arr));
 	return a;
 }
+
+/*
+
+CODING CHALLANGE 12
+
+Power Calculator
+Create a function that takes voltage and current and returns the calculated power.
+
+Examples
+circuitPower(230, 10) ➞ 2300
+
+circuitPower(110, 3) ➞ 330
+
+circuitPower(480, 20) ➞ 9600
+*/
+
+function circuitPower(voltage, current) {
+	return voltage * current;
+}
+
+/*
+
+CODING CHALLANGE 13
+
+Promises III: Native Promise, Introducing the Executor
+Promises are just objects that contain the outcome of asynchronous operations. So when do you use one? When you want to control the outcome of an asynchronous operation. All you have to do is wrap the asynchronous function with a promise constructor.
+
+The promise constructor requires you to pass a function called the executor which takes two parameters, resolve and reject. Both are functions that you use to pass or reject a value that is usually the result of the async operation. Here's an example of a simple promise:
+
+let promise = new Promise( (resolve, reject) => {
+  setTimeout(( ) => {
+     resolve("edabit")
+  }, 1000)
+})
+setTimeout is a browser API that is very commonly used in tutorials to represent async operations. After 1000ms has passed, we call the callback function in setTimeout() and pass a string "edabit" to the resolve function.
+
+Create a simple promise and pass the resolve function a string value of your choice. Use the setTimeout function as your asynchronous operation. Your setTimeout() function should not exceed 1000ms. Store the promise inside a variable named promise.
+*/
+
+let promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('edabit');
+	}, 1000);
+});
+
+/*
+
+CODING CHALLANGE 14
+
+Matchstick Houses
+This challenge will help you interpret mathematical relationships both algebraically and geometrically.
+
+Matchstick Houses, Steps 1, 2 and 3
+
+Create a function that takes a number (step) as an argument and returns the number of matchsticks in that step. See step 1, 2 and 3 in the image above.
+
+Examples
+matchHouses(1) ➞ 6
+
+matchHouses(4) ➞ 21
+
+matchHouses(87) ➞ 436
+
+*/
+
+function matchHouses(steps) {
+	return steps * 5 + 1;
+}
