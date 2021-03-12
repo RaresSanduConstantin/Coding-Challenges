b0fbe09198adf62c2e0a00b790a1b1124d5c08d3
Rares Sandu <sandurares57@gmail.com>
2021-03-12 09:14:47 +0200

    firday theen

--- app.js
+++ app.js
@@ -782,3 +782,26 @@ var isSquare = function(n) {
 	if (n === 0) return true;
 	return n > 0 && Math.sqrt(n) % 1 === 0;
 };
+
+
+/*
+CODING CHALLANGE 31
+Sum of Cubes
+Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+
+Assume that the input n will always be a positive integer.
+
+Examples:
+
+
+*/
+
+function sumCubes(n){
+    let sum = [];
+   for(let i=1; i<=n; i++){
+     let a = i**3
+  //    console.log(a)
+     sum.push(a)
+   }
+    return sum.reduce((acc, cur) => acc+cur)
+  }
