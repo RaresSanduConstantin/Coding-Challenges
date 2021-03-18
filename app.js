a0c0028ab90a2ea9da0c8d697fec777b326fc1c5
Rares Sandu <sandurares57@gmail.com>
2021-03-18 09:35:13 +0200

    thursday coding

--- app.js
+++ app.js
@@ -881,3 +881,38 @@ function findShort(s){
                            })
     return Math.min.apply(Math, lengtharr)
   }
+
+  /*
+   CODING CHALLANGE 34
+Exes and Ohs
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+  */
+
+function XO(str) {
+    //code here
+  let arr = str.toLowerCase().split('')
+  let x = 0;
+  let o = 0;
+  arr.forEach(c => {
+
+    if(c === 'x'){
+      x++
+    } else if(c === 'o'){
+    o++
+    }
+  })
+
+  if(x === o){
+    return true
+  } else {
+    return false
+  }
+}
