eb76dfe789e49c6be21ab52b634316503e4ee4a6
Rares Sandu <sandurares57@gmail.com>
2021-04-09 14:49:40 +0300

    friday coding

--- app.js
+++ app.js
@@ -1115,3 +1115,80 @@ function paul(x){
         return 'Miserable!'
       }
     }
+
+    /*
+    CODING CHALLANGE 42
+
+    In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+At the end of the first year there will be:
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+At the end of the 2nd year there will be:
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years.
+More generally given parameters:
+
+p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+Examples:
+nb_year(1500, 5, 100, 5000) -> 15
+nb_year(1500000, 2.5, 10000, 2000000) -> 10
+Note:
+Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+    */
+
+function nbYear(p0, percent, aug, p) {
+    let count = 0;
+    while(p0<p){
+        p0 += p0 * percent/100 + aug;
+        count++;
+    }
+    return count;
+}
+
+
+/*
+ CODING CHALLANGE 43
+Drink about
+Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"
+*/
+
+function peopleWithAgeDrink(age){
+    if(age < 14)
+      drink = 'toddy';
+    else if(age < 18)
+      drink = 'coke'
+    else if(age < 21)
+      drink = 'beer';
+    else if(age => 21)
+      drink = 'whisky';
+
+    return 'drink ' + drink;
+  }
