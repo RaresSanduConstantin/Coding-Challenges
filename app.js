c28f0288a09559e7a219993ca3b49e0b2dbaa108
Sandu <sandurares57@gmail.com>
2021-04-23 09:12:26 +0300

    katas

--- app.js
+++ app.js
@@ -1257,3 +1257,109 @@ function validSpacing(s) {
     return !s.split(" ").includes("")
 }
   
+
+/* 
+Kata - 21.04.2021
+
+Word to binary
+
+Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. The binary strings should be eight digits long.
+
+Example: 'man' should return [ '01101101', '01100001', '01101110' ]
+
+*/
+function wordToBin(str){
+  //code away!!!
+  let arr = str.split('')
+  return arr.map(char => {
+    return '0' + char.charCodeAt(0).toString(2)
+  })
+}
+  
+//   Matematica binari in 8 bites
+//  return   arr.map(char => {
+// let bite = new Array(8).fill(0)
+//     let asciiCode = char.charCodeAt(0);
+//     bite.forEach((elm, i) => {
+//     if(Math.pow(2, i) < asciiCode){
+//       asciiCode = asciiCode - Math.pow(2, i)
+//       console.log(asciiCode)
+//       elm++;
+//     }
+//     }
+//   )
+//     return bite.join('')
+//   })
+
+
+/* 
+Kata - 22.04.2021
+
+Sort Numbers
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/  
+
+function solution(nums){
+if(!nums){
+  return []
+} else {
+  return nums.sort((a, b) => a-b)
+}
+}
+
+/* 
+
+Snail Sort
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+       [4,5,6],
+       [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+For better understanding, please follow the numbers of the next array consecutively:
+
+array = [[1,2,3],
+       [8,9,4],
+       [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+*/
+
+const array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+//varianta 1
+const array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+//varianta 1
+const snail = (array) =>{
+debugger
+let finalArray = []
+while(array.length){
+  finalArray.push(...array.shift())
+  for (var i = 0; i < array.length; i++){
+    finalArray.push(array[i].pop())
+  }
+  finalArray.push(...(array.pop() || []).reverse())
+  for (var i = array.length -1; i >= 0; i--){
+    finalArray.push(array[i].shift())
+  }
+}
+return finalArray
+}
+// snail(array)
+//varianta 2
+
+const snail = (arr) => {
+var finalArray = [];
+while (arr.length) {
+  finalArray.push(...arr.shift());
+  arr.map(row => finalArray.push(row.pop()))
+  arr.reverse().map(row => row.reverse());
+}
+return finalArray
+}
